19018feddce751093472ad0f51948c528571396c
anton <antonsashkov@gmail.com>
2022-08-24 20:17:30 +0200

    add spring data rest

--- src/main/java/com/sashkou/springdata/dao/mongo/repository/UserRepository.java
+++ src/main/java/com/sashkou/springdata/dao/mongo/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.sashkou.springdata.dao.mongo.repository;
 import com.sashkou.springdata.service.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends MongoRepository<User, Long> {
 
     List<User> findAll(Sort sort);
